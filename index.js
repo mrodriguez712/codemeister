f5e3a5fbbce5c2c493b86a6ea60a160647591490
Marte Rodriguez <marte.rod0712@gmail.com>
2023-03-14 09:26:41 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+import { works } from './projects.js'
+
+const dateEl = document.getElementById('date-el')
+const numberOfWorks = document.getElementById('number-of-works')
+const projectsEl = document.getElementById('projects-el')
+
+// date
+let date = new Date()
+let getMonth = date.toLocaleString('default', { month: 'long' })
+let getYear = date.getFullYear()
+
+dateEl.innerHTML =
+    `
+    <h4 class="date">
+    ${getMonth} 
+    <br/>
+    ${getYear}
+    </h4>
+`
+// project number
+numberOfWorks.textContent = works.length
+
+// projects
+for (let work of works) {
+    projectsEl.innerHTML =
+    `
+    <h2>${work.name}</h2>
+    <p>${work.desc}</p>
+    <p>${work.linkdesc} 
+    <a href='${work.link}'>HERE
+    </p>
+    `
+}
+
+
+
+
+
+
---
+++ projects.js
@@ -0,0 +1,8 @@
+export const works = [
+    {
+        name: 'Project 1: Make a Cake',
+        desc: "I built this through an online course and added couple of JavaScript functions",
+        linkdesc: "Check out the live demo",
+        link: "https://mrodriguez712.github.io/makeacake/"
+    }
+]
